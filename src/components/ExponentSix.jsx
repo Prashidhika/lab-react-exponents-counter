563fa3bb4961c135fc9db102950ceb8af0d85bb1
Prashidhika Neupane <mununeupane8@gmail.com>
2024-08-04 00:21:36 +0100

    done

--- src/components/ExponentSix.jsx
+++ src/components/ExponentSix.jsx
@@ -1,7 +1,12 @@
-const ExponentSix = () => (
+import React from "react";
+
+const ExponentSix = ({ count }) => (
   <div className="exponent-counter-container">
     <p className="exponent-label">n⁶</p>
-    <p className="exponent-result">2 * 2 * 2 * 2 * 2 * 2 = <span className="total">64</span></p>
+    <p className="exponent-result">
+      {count} * {count} * {count} * {count} * {count} * {count} =
+      <span className="total">64</span>
+    </p>
   </div>
 );
 
